f1d6109622ac66e8e55ed3e5dd12f6c2d81e9620
Nurul Imran <nurulimran696@gmail.com>
2025-11-04 17:17:28 +0600

    Add Explore Menu Section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import Banner from '../../components/Banner/Banner';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 const Home = () => {
+    const [category, setCategory] = useState("all");
     return (
         <>
             <Banner />
+            <ExploreMenu category={category} setCategory={setCategory} />
         </>
     )
 }
