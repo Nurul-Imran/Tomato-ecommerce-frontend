82f1469bbe7857aefd7bcaee986f8407eb1b4540
Nurul Imran <nurulimran696@gmail.com>
2025-11-05 03:53:39 +0600

    Add Side Menu in Navbar Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoMenu } from "react-icons/io5";
+import { ImCross } from "react-icons/im";
 
 import { assets } from "../../assets/assets";
 import "./Navbar.css";
 
 const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState("home");
-
+  const [openSideMenu, setOpenSideMenu] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-   
+
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
-      if (offset > 80) { 
+      if (offset > 80) {
         setScrolled(true);
       } else {
         setScrolled(false);
@@ -22,9 +23,9 @@ const Navbar = () => {
 
     window.addEventListener("scroll", handleScroll);
 
-    // Cleanup on component unmount 
+    // Cleanup on component unmount
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [])
+  }, []);
   return (
     <nav id="nav" className={`${scrolled ? "scrolled" : ""}`}>
       <div className="container">
@@ -73,7 +74,54 @@ const Navbar = () => {
             <button>Sing in</button>
           </div>
           <div className="side_menu_bar">
-            <IoMenu />
+            <IoMenu onClick={() => setOpenSideMenu(true)} />
+            {
+              openSideMenu && (
+                <div className="side_menu">
+                  <ul className="menu">
+                    <li
+                      className={activeMenu === "home" ? "active" : ""}
+                      onClick={() => setActiveMenu("home")}
+                    >
+                      <Link to="/">home</Link>
+                    </li>
+                    <li
+                      className={activeMenu === "menu" ? "active" : ""}
+                      onClick={() => setActiveMenu("menu")}
+                    >
+                      <a href="#explore_menu">Menu</a>
+                    </li>
+                    <li
+                      className={activeMenu === "mobile-app" ? "active" : ""}
+                      onClick={() => setActiveMenu("mobile-app")}
+                    >
+                      <a href="#download_app">mobile app</a>
+                    </li>
+                    <li
+                      className={activeMenu === "contact-us" ? "active" : ""}
+                      onClick={() => setActiveMenu("contact-us")}
+                    >
+                      <a href="#footer">contact us</a>
+                    </li>
+                  </ul>
+                  <div className="menu_right">
+                    <div className="search_icon">
+                      <img src={assets.search_icon} alt="Search Icon" />
+                    </div>
+                    <div className="cart_icon_box">
+                      <Link>
+                        <img src={assets.basket_icon} alt="Cart Icon" />
+                      </Link>
+                      <div className="dot"></div>
+                    </div>
+                    <button>Sing in</button>
+                  </div>
+                  <div className="cross">
+                    <ImCross onClick={() => setOpenSideMenu(false)} />
+                  </div>
+                </div>
+              )
+            }
           </div>
         </div>
       </div>
